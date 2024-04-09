5ef95ae4a1537610bdedb07c31e22a4d05c6aa39
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-04-09 18:22:11 +0200

    ADD: creating new project with vite-plus-bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,41 @@
+<script>
+export default{
+  data(){
+    return{
+      message: 'La mia prima app con Vite!!!!'
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <figure>
+    <img src="https://th.bing.com/th/id/R.a19c5b93d037fb0bd0600c6e1603765d?rik=QrMkvf6AkoWsCA&pid=ImgRaw&r=0" alt="Surfer on a wave">
+    <figcaption>KAWABOOOOOOOONGAAA!</figcaption>
+  </figure>
+
+</template>
+
+<style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+img {
+  width: 100%;
+}
+figure{
+  position: relative;
+}
+figcaption{
+  font-size: 70px;
+  position: absolute;
+  top: 70%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,44 @@
+<script>
+export default{
+  data(){
+    return{
+      message: 'La mia prima app con Vite!!!!',
+      counter:0
+    }
+  }
+}
+</script>
+
+<template>
+  <body>
+    <header>
+
+     <h1>La mia prima App con Vite!!!!!!!!</h1>
+
+    </header>
+  </body>
+</template>
+
+<style scoped>
+
+
+h1 {
+  
+  color: orange;
+  font-size: 68px;
+  text-align: center;
+  padding: 24px;
+  
+  
+}
+header{
+height: 200px;
+background-image: url("https://i.pinimg.com/originals/ce/62/24/ce6224b531011346757c43ef95e681dd.jpg");
+background-position: top;
+}
+
+
+
+
+
+</style>
